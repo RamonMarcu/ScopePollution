15b11407e3f09a38917035f11d316b550530947c
Ramon Marcu <ramondotmarcu@gmail.com>
2021-06-29 17:11:54 +0300

    Add files via upload

---
+++ ScopePollution.js
@@ -0,0 +1,21 @@
+//main.js
+
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  stars = 'Sirius';
+  
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars);
+
+/* 
+
+Night Sky: The Moon, Sirius, The Milky Way
+Sirius
+
+*/
